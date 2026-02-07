ab296af34fe9a68839b3be783c04021657867330
xucx1 <353209278@qq.com>
2026-02-07 21:17:33 +0800

    店铺营业状态设置-业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -21,7 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/admin/setmeal")
-@Api("套餐相关接口")
+@Api(tags = "套餐相关接口")
 @Slf4j
 public class SetmealController {
     @Autowired
---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,40 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 店铺管理
+ * RestController：声明这是一个 REST 接口控制器，返回值直接作为响应数据，自定义Bean的名称，避免两个shopController冲突
+ * RequestMapping：定义请求路径和请求方式，实现 URL 到方法的映射
+ */
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取到店铺的营业状态为：{}", status == 1 ? "营业中" : "已打烊");
+
+        return Result.success(status);
+    }
+}
