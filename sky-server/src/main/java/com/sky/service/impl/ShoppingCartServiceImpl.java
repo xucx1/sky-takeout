e137c692409395c6c9e63af6ace385cb0b5269f1
xucx1 <353209278@qq.com>
2026-02-10 16:31:00 +0800

    查看购物车-业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -44,27 +45,44 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             item.setNumber(item.getNumber() + 1);
             // 执行update语句更新数据库
             shoppingCartMapper.updateNumberById(item);
-        }
-        // 如果不存在，执行insert
-        Long dishId = shoppingCart.getDishId();
-        Long setmealId = shoppingCart.getSetmealId();
-        // 判断本次添加的是菜品还是套餐
-        if (dishId != null) {
-            // 添加的是菜品
-            Dish dish = dishMapper.getById(dishId);
-            shoppingCart.setName(dish.getName());
-            shoppingCart.setAmount(dish.getPrice());
-            shoppingCart.setImage(dish.getImage());
         } else {
-            // 添加的是套餐
-            Setmeal setmeal = setmealMapper.getById(setmealId);
-            shoppingCart.setName(setmeal.getName());
-            shoppingCart.setAmount(setmeal.getPrice());
-            shoppingCart.setImage(setmeal.getImage());
-        }
-        shoppingCart.setNumber(1);
-        shoppingCart.setCreateTime(LocalDateTime.now());
+            // 如果不存在，执行insert
+            Long dishId = shoppingCart.getDishId();
+            Long setmealId = shoppingCart.getSetmealId();
+            // 判断本次添加的是菜品还是套餐
+            if (dishId != null) {
+                // 添加的是菜品
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setImage(dish.getImage());
+            } else {
+                // 添加的是套餐
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setAmount(setmeal.getPrice());
+                shoppingCart.setImage(setmeal.getImage());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
 
-        shoppingCartMapper.insert(shoppingCart);
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        return list;
     }
 }
